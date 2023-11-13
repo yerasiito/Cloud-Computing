228ee746aedb76ddb26a6f78119d8abcf62bfbb2
Daniel Müller <daniel.q.mueller@stud.h-da.de>
2023-11-13 15:28:06 +0000

    Update main.tf
    - Add gitlab http backend for terraform state

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 terraform {
-required_version = ">= 0.14.0"
+  backend "http" {
+    address        = "https://code.fbi.h-da.de/api/v4/projects/31281/terraform/state/tf_state"
+    lock_address   = "https://code.fbi.h-da.de/api/v4/projects/31281/terraform/state/tf_state/lock"
+    unlock_address = "https://code.fbi.h-da.de/api/v4/projects/31281/terraform/state/tf_state/lock"
+    username       = "gitlab-ci-token"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+  }
+
+  required_version = ">= 0.14.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
