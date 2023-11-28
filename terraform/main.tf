1f2f64ec62eb8df76979232c6c124c6a4fdcbdc8
Kai-Philipp Nosper <kai-philipp.nosper@stud.h-da.de>
2023-11-28 08:42:02 +0100

    Add security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,24 @@
+resource "openstack_networking_secgroup_v2" "secgroup_grafana" {
+  name        = "secgroup_grafana"
+  description = "Terraform security group for grafana"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_grafana" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 3000
+  port_range_max    = 3000
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup_grafana.id
+}
+
 resource "openstack_compute_instance_v2" "test-server-tf" {
   name            = "test-server-tf"
   image_id        = var.openstack_image_id_debian 
   flavor_id       = var.openstack_flavor_id_m1_small
   key_pair        = var.ssh_key_pair_name
-  security_groups = ["default","allow-ssh","allow-http"]
+  security_groups = ["default","allow-ssh","allow-http","secgroup_grafana"]
 
   network {
     name = var.openstack_network_name
@@ -26,3 +41,4 @@ resource "local_file" "tf_ansible_vars_file_new" {
   DOC
   filename = "./tf_ansible_vars_file.yml"
 }
+
