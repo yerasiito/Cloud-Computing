39de5b93067ab6b64caaf07712c0b6210124b88a
Kai-Philipp Nosper <kai-philipp.nosper@stud.h-da.de>
2023-11-28 10:26:58 +0000

    Update inventory.py

--- ansible/inventory.py
+++ ansible/inventory.py
@@ -15,9 +15,8 @@ def write_to_inventory(ip_addresses, inventory_path):
         for idx, ip in enumerate(ip_addresses, start=1):
             inventory_file.write(f"      ansible_host: {ip}\n")
 
-gitlab_path = os.getenv('CI_PROJECT_DIR')
-print(gitlab_path)
-file_path = gitlab_path + '/terraform/tf_ansible_vars_file.yml'
+
+file_path = gitlab_path + '/builds/cloud-computing-technology/lab/2023_ws/group_b_8/terraform/tf_ansible_vars_file.yml'
 inventory_path = 'inventory.yml'
 
 ip_addresses = get_ip_addresses(file_path)
